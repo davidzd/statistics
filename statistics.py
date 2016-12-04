c9380adb476c2b5895fc2a5efd14d6a9ac85133a
davidzd <davidzd@163.com>
2016-12-04 17:25:55 +0800

    lal

--- statistics.py
+++ statistics.py
@@ -5,8 +5,10 @@ server = couchdb.Server('http://127.0.0.1:5984/')
 db = server['cloud_tweets_world']
 
 sum  = 0
+count = 0
 for i in db:
+    if count%10000 == 0:
+        print sum
     if 'retweet_count' in db[i]:
         sum += db[i]['retweet_count']
-
 print sum
