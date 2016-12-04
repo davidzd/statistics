e6d8b45c6b12e07016e7bc87618a058048637a95
davidzd <davidzd@163.com>
2016-12-04 19:13:41 +0800

    haha

--- statistics.py
+++ statistics.py
@@ -1,14 +1,59 @@
 # python
 import couchdb
+import json
 
 server = couchdb.Server('http://127.0.0.1:5984/')
 db = server['cloud_tweets_world']
 
 sum  = 0
 count = 0
+td = dict()
+fc = dict(follower=dict(), friend=dict())
+td['weekend'] = dict()
+td['weekday'] = dict()
+user_list = []
+gap = 50
+
 for i in db:
+    # print loading
     if count%10000 == 0:
-        print sum
-    if 'retweet_count' in db[i]:
-        sum += db[i]['retweet_count']
-print sum
+        print sum, td, fc
+
+    # sentiment distribution.
+    if 'sentiment' in db[i] and type(db[i]['sentiment']) == int:
+        sum += db[i]['sentiment']
+
+    # time distribution
+    if 'created_at' in db[i]:
+        day = db[i]['created_at'].split(' ')[0]
+        hour = str(db[i]['created_at'].split(' ')[3].split(':')[0])
+        if day in ['Sat', 'Sun']:
+            sig = 'weekend'
+        else:
+            sig = 'weekday'
+
+        if hour in td[sig]:
+            td[sig][hour] += 1
+        else:
+            td[sig][hour] = 0
+
+    # follower count
+    if 'user_followers_count' in db[i] and 'user_friends_count' in db[i] and 'user_id' in db[i]:
+        if db[i]['user_id'] not in user_list:
+            user_list.append(db[i]['user_id'])
+            if str((db[i]['user_followers_count'])/gap) in fc['follower']:
+                fc['follower'][str((db[i]['user_followers_count'])/gap)] += 1
+            else:
+                fc['follower'][str((db[i]['user_followers_count']) / gap)] = 1
+
+            if str((db[i]['user_friends_count'])/gap) in fc['friend']:
+                fc['friend'][str((db[i]['user_friends_count']) / gap)] += 1
+            else:
+                fc['friend'][str((db[i]['user_friends_count']) / gap)] = 1
+    count += 1
+f = open('statistics.txt', 'wb')
+f.write(json.dumps(td))
+f.write(str(sum))
+f.write(json.dumps(fc))
+f.close()
+print td, sum, fc
