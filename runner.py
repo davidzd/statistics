c6b62ef4e6e0ebd65a06cd40fec8391051a562db
Da Zhang <davidzd@163.com>
2017-01-11 18:05:32 +0800

    1111

---
+++ runner.py
@@ -0,0 +1,16 @@
+# runner.py
+from event import events_list
+def run(tasks):
+    for task in tasks:
+        _next(task)
+    while len(events_list):
+        for event in events_list:
+            if event.is_ready():
+                events_list.remove(event)
+                break
+def _next(task):
+    try:
+        event = next(task)
+        event.set_callback(lambda: _next(task)) # 1
+    except StopIteration:
+        pass
